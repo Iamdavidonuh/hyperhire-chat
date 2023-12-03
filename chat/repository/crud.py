fbdfe820bc708e1fcbf5e25e67dc70476d9bfc19
David Onuh <davidonuh1@gmail.com>
2023-12-03 06:26:56 +0100

    add create_text_message function

--- chat/repository/crud.py
+++ chat/repository/crud.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.conf import settings
 from rest_framework import exceptions as rest_execeptions, status
-from chat.models import ChatRooms
+from chat.models import ChatRooms, Message
+from . import constants
 
 
 def add_users_to_chatroom(user: User, room_name: str) -> ChatRooms:
@@ -17,3 +18,9 @@ def leave_chat_room(user: User, room_name: str) -> ChatRooms:
     chat_room = ChatRooms.objects.get(room_name=room_name)
 
     return chat_room.leave_room(user=user)
+
+
+def create_text_message(sender: User, chat_room: ChatRooms, content: str):
+    return Message.objects.create(
+        message_type=constants.MessageType.TEXT, room=chat_room, text=content
+    )
