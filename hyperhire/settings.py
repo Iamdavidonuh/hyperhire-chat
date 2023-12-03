77ee66a4c3401c60772cc6e8cd9fe049d422f83c
David Onuh <davidonuh1@gmail.com>
2023-12-03 08:01:43 +0100

    set default permissions for api

--- hyperhire/settings.py
+++ hyperhire/settings.py
@@ -140,3 +140,12 @@ CHANNEL_LAYERS = {
 
 
 MAX_MEMBERS_IN_ROOMS = 30
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ]
+}
