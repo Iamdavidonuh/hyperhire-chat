86b990b732c0aef1e994d4e3dc13b3643c4d279b
David Onuh <davidonuh1@gmail.com>
2023-12-03 11:55:58 +0100

    add logout view

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", include("chat.api.urls")),
+    path("logout/", views.signout, name="logout"),
     path("chat/", views.index, name="index"),
     path("chat/<str:room_name>/", views.room, name="room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.urls import reverse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,5 +10,11 @@ def index(request):
     return render(request, "chat/index.html")
 
 
+@login_required
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+
+
+def signout(request):
+    logout(request)
+    return redirect(reverse("index"))
